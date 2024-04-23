7c6c75edd0bafeb5601b7b3f1bd2fc13e3b26863
Luisrivas29 <143302710+Luisrivas29@users.noreply.github.com>
2024-04-22 18:48:56 -0600

    Add files via upload

---
+++ EVA3_1_EXCEPCIONES/src/eva3_1_excepciones/EVA3_1_EXCEPCIONES.java
@@ -0,0 +1,19 @@
+
+package eva3_1_excepciones;
+
+import java.util.Scanner;
+
+
+public class EVA3_1_EXCEPCIONES {
+
+    
+    public static void main(String[] args) {
+        int num;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Introduce un valor entero: ");
+        num = captu.nextInt();
+        System.out.println("El valor es " + num);
+        
+    }
+    
+}
