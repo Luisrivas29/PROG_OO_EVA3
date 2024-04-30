b73d4731c7529186e132b8ed19f9439c4c4c1f43
Luisrivas29 <143302710+Luisrivas29@users.noreply.github.com>
2024-04-29 18:47:27 -0600

    Add files via upload

---
+++ EVA3_7_USO_EXCEPCION/src/eva3_7_uso_excepcion/EVA3_7_USO_EXCEPCION.java
@@ -0,0 +1,40 @@
+
+package eva3_7_uso_excepcion;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class EVA3_7_USO_EXCEPCION {
+
+   
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Introduce un numero entero: ");
+        int num = input.nextInt();
+        boolean valor = false;
+        while(valor == false){
+        try{
+            
+        } catch(InputMismatchException e) {
+            
+        }
+        break;
+        }
+           
+       }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+
